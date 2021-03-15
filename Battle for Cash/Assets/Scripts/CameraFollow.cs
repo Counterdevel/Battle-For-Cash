f7198751d4731e2e3ad40523b57f2c11aea8baae
luisRcarvalho <54163895+luisRcarvalho@users.noreply.github.com>
2021-03-15 19:37:28 -0300

    Device Simulator e Teste de Move

---
+++ Battle for Cash/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed;
+
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredpos = target.position + offset;
+        Vector3 smoothedpos = Vector3.Lerp(transform.position, desiredpos, smoothSpeed);
+        transform.position = smoothedpos;
+        transform.LookAt(target);
+    }
+}
---
+++ Battle for Cash/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private VariableJoystick joystick;
+    public Rigidbody rbPlayer;
+
+    public float playerspeed, forcejump;
+
+    public bool isGround;
+
+    Vector3 direction, rotate;
+    void Start()
+    {
+        joystick = GameObject.Find("Variable Joystick").GetComponent<VariableJoystick>();
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MoveMobile();
+        Rotation();
+    }
+
+    void MoveMobile()
+    {
+        direction = (Vector3.forward * joystick.Vertical) + (Vector3.right * joystick.Horizontal);
+    }
+    void Rotation()
+    {
+        if(direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        transform.Translate(direction * (playerspeed * Time.deltaTime), Space.World);
+    }
+
+    public void Jump()
+    {
+        if (isGround)
+        {
+            rbPlayer.AddForce(Vector3.up * forcejump, ForceMode.Impulse);
+            isGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+}
