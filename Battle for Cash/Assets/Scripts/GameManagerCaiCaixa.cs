a056f02f5a18ea055bf2da221b420dfe78986856
Henrique <henriqueanselmo1011@gmail.com>
2021-03-29 22:32:01 -0300

    Começo do script gamemanager jogo da caixa
    Já feito o spawn random das caixas, usar botão direito para testar

---
+++ Battle for Cash/Assets/Scripts/GameManagerCaiCaixa.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerCaiCaixa : MonoBehaviour
+{
+
+    public GameObject PrefabCaixa;
+     GameObject CaixaTimerComponents;
+
+    public int CaixaSpawnChanceX;
+    public int CaixaSpawnChanceY;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+
+            CaixaSpawnChanceX = Random.Range(-10, 10);
+            CaixaSpawnChanceY = Random.Range(-10, 10);
+
+            CaixaTimerComponents = Instantiate(PrefabCaixa, new Vector3(CaixaSpawnChanceX, 10, CaixaSpawnChanceY), Quaternion.identity);
+            Destroy(CaixaTimerComponents.GetComponent<Rigidbody>(),5);
+        }
+    }
+}
