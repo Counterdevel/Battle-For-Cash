3c7f5c9201f5b6611a099fbcaaf880e0212360fd
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-04-10 23:45:18 -0300

    timer
    tic tac go

---
+++ Battle for Cash/Assets/CountDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    public GameObject startCountdown;
+    void Start()
+    {
+        StartCoroutine("StartDelay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        float timepause = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < timepause)
+            yield return 0;
+        startCountdown.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
