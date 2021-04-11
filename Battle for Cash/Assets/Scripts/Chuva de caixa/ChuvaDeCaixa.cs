c568c7723fd6c3ca30019d1d94683d8a77089de1
Henrique <henriqueanselmo1011@gmail.com>
2021-04-11 18:11:58 -0300

    Adicionado Prop de barril e Melhora no script de chuva de caixas(LER DESCRICAO)
    -Script chuvadecaixas agora para de tentar spawnar caixas e travar o game quando a cena enche de caixas.

--- Battle for Cash/Assets/Scripts/Chuva de caixa/ChuvaDeCaixa.cs
+++ Battle for Cash/Assets/Scripts/Chuva de caixa/ChuvaDeCaixa.cs
@@ -26,8 +26,13 @@ public class ChuvaDeCaixa : MonoBehaviour
         x = Random.Range(0, 11);
         z = Random.Range(0, 8);
         Vector3 position = new Vector3(positionsx[x], 35, positionsz[z]);
-
-        if(PosicoesJaSalvas.Contains(position))
+        float tamanholist = PosicoesJaSalvas.Count;
+        Debug.Log(tamanholist);
+        if (tamanholist == 88) 
+        {
+            CancelInvoke("RespawnCaixa");
+        }
+        else if (PosicoesJaSalvas.Contains(position))
         {
             Debug.Log("Posicao ja existe");
             RespawnCaixa();
