5e70db4e8f510aff9d557a6a1617d48e8c8c7342
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-04-16 17:04:31 -0300

    testes

--- Battle for Cash/Assets/Scripts/Chuva de caixa/Caixa.cs
+++ Battle for Cash/Assets/Scripts/Chuva de caixa/Caixa.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Caixa : MonoBehaviour
 {
+    public static string eliminado1;
+    public static string eliminado2;
+    public static string eliminado3;
+    int i = 0;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.gameObject.SetActive(false);
+            if(i == 0)
+            {
+                eliminado1 = other.gameObject.name;
+                i++;
+            }
+            if (i == 1)
+            {
+                eliminado2 = other.gameObject.name;
+                i++;
+            }
+            if (i == 2)
+            {
+                eliminado3 = other.gameObject.name;
+                i++;
+            }
+
         }
     }
+
+
 }
