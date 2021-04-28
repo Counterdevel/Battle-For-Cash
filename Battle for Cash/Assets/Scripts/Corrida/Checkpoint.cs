08d1209352939e661d01f50e20e213fc7bc7fe47
Pierre Muller <pierreeitel@hotmail.com>
2021-04-28 20:30:35 -0300

    Capture a bandeira progredindo

--- Battle for Cash/Assets/Scripts/Corrida/Checkpoint.cs
+++ Battle for Cash/Assets/Scripts/Corrida/Checkpoint.cs
@@ -8,7 +8,6 @@ public class Checkpoint : MonoBehaviour
     GameObject player;
     void Start()
     {
-
         player = GameObject.FindWithTag("Player");
     }
 
--- Battle for Cash/Assets/Scripts/Player/PlayerFlag.cs
+++ Battle for Cash/Assets/Scripts/Player/PlayerFlag.cs
@@ -6,26 +6,27 @@ public class PlayerFlag : MonoBehaviour
 {
     public GameObject bandeiraRoubada;
     public GameObject socket;
-    bool bandeirado = false;
+    bool flagado = false;
 
+    private void Start()
+    {
+        bandeiraRoubada = GameObject.FindGameObjectWithTag("Flag");
+    }
     public void PickUpFlag(GameObject flag)
     {
         flag.transform.position = socket.transform.position;
         flag.transform.parent = socket.transform;
-        bandeirado = true;
+        flagado = true;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Player" && bandeirado == false)
+        if(collision.gameObject.tag == "Player" && flagado == false)
         {
             bandeiraRoubada.transform.position = socket.transform.position;
             bandeiraRoubada.transform.parent = socket.transform;
+            flagado = true;
         }
     }
 
-    public void Ladrao(bool flagado)
-    {
-
-    }
 }
