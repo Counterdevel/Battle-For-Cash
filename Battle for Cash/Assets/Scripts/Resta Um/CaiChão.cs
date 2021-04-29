7ec43bbb8774221835de8ef2e153b03d875f3891
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-04-29 17:53:33 -0300

    Resta um - Script do cai chão

---
+++ Battle for Cash/Assets/Scripts/Resta Um/CaiChão.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CaiChão : MonoBehaviour
+{
+    public List<GameObject> chao;
+    public float inicioChuva = 3f;
+    public float intervalo = 5f;
+    void Start()
+    {
+        InvokeRepeating("caichão", inicioChuva, intervalo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [PunRPC]
+    void caichão()
+    {
+        int index = Random.Range(0, chao.Count);
+        chao[index].GetComponent<MeshRenderer>().material.color = Color.red;
+        chao[index].GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
