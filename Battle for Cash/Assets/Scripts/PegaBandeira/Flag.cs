685392b1c287ffd9c294e80fb37da4034a64d7bd
Pierre Muller <pierreeitel@hotmail.com>
2021-05-11 21:52:08 -0300

    flag

--- Battle for Cash/Assets/Scripts/PegaBandeira/Flag.cs
+++ Battle for Cash/Assets/Scripts/PegaBandeira/Flag.cs
@@ -17,10 +17,13 @@ public class Flag : MonoBehaviour
 
     GameObject socket;
 
+    bool flagCaptured = false;
+
     private void Start()
     {
         socket = GameObject.FindWithTag("Socket");
     }
+    [PunRPC]
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -28,15 +31,26 @@ public class Flag : MonoBehaviour
             gameObject.transform.position = socket.transform.position;
             gameObject.transform.rotation = socket.transform.rotation;
             gameObject.transform.parent = socket.transform;
+            flagCaptured = true;
         }
     }
 
+    [PunRPC]
     public void RespawnFlag(bool lost)
     {
         if(lost == true)
         {
             gameObject.transform.parent = null;
-            gameObject.transform.position = transform.position = new Vector3(Random.Range(-5f, -81.2f), 0f, Random.Range(32f, -56.5f));
+            PhotonNetwork.Instantiate(gameObject.name, transform.position = new Vector3(Random.Range(-5f, -81.2f), 0f, Random.Range(32f, -56.5f)), transform.rotation);
+        }
+    }
+
+    public void StolenFlag(bool stolen)
+    {
+        if(stolen == true && flagCaptured == true)
+        {
+            gameObject.transform.parent = null;
+            gameObject.transform.parent = socket.transform;
         }
     }
 }
--- Battle for Cash/Assets/Scripts/Player/PlayerFlag.cs
+++ Battle for Cash/Assets/Scripts/Player/PlayerFlag.cs
@@ -1,8 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerFlag : MonoBehaviour
 {
-    
+    PhotonView photonView;
+
+    private void Start()
+    {
+        photonView.GetComponent<PhotonView>();
+    }
+
+    private void Update()
+    {
+        if(!photonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            Flag.Instance.StolenFlag(true);
+        }
+    }
 }
